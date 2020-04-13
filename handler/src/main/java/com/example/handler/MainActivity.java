434b4e8832dba5866ecb280bd92dc81921e77cbd
danke <danke@2dfire.com>
2020-04-13 15:47:39 +0800

    [MOD] handler 冲突

--- handler/src/main/java/com/example/handler/MainActivity.java
+++ handler/src/main/java/com/example/handler/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.view.View;
 
