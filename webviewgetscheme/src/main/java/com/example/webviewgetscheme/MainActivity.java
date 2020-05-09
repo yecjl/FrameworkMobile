bda239f3da55d437bc3e0a336066cf30d5425bd1
danke <danke@2dfire.com>
2020-05-09 15:07:36 +0800

    [MOD] 增加下载监听

--- webviewgetscheme/src/main/java/com/example/webviewgetscheme/MainActivity.java
+++ webviewgetscheme/src/main/java/com/example/webviewgetscheme/MainActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.webkit.DownloadListener;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -22,20 +25,45 @@ public class MainActivity extends AppCompatActivity {
         // 通过报错ERR_UNKNOWN_URL_SCHEME，获取应用scheme, 比如抖音的是 snssdk1128:// （也可以反编译，或者百度）
         WebView webView = findViewById(R.id.web);
         setWebSettings(webView.getSettings());
+//        webView.loadUrl("https://ad-bussiness-thirdparty.e.kuaishou.com/#/merchant?from=5&code=1486&ticket=99931828");
         webView.loadUrl("https://v.douyin.com/KWVABo/"); // 抖音分享链接 -- 点击打开抖音，就会报错ERR_UNKNOWN_URL_SCHEME
+//        webView.loadUrl("https://d.2dfire.com/micropages/live-detail.html?entity_id=00045856&business_id=00045856&hdt=1588838700617&oAuthHandlerCode=MicroMessengerdirect&profession=0&sdt=1588838700584&industry=0&shop_kind=1&shop_name=sweet&l=1&token=7094ffa002ac3b8ba19e0015eb8b2c78&uid=4b9fdef3acf4401caadc7c0a6fc02e04&v=1&nickname=Vicky&qr_code=14&client=1&openAppId=377068829077031622&appVersion=1.0.0&isvFlag=0"); // 抖音分享链接 -- 点击打开抖音，就会报错ERR_UNKNOWN_URL_SCHEME
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+                Log.d("danke", "shouldOverrideUrlLoadingListener: " + url);
 //                if (url.startsWith("http://") || url.startsWith("https://")) {
-                view.loadUrl(url);
+                    view.loadUrl(url);
 //                } else {
 //                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 //                    startActivity(intent);
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+//                intent.setData(Uri.parse(url));
+//                startActivity(intent);
 //                }
                 return super.shouldOverrideUrlLoading(view, url);
             }
         });
+        webView.setDownloadListener(getDownloadListener());
+    }
+
+    /**
+     * 下载文件监听，跳转浏览器
+     */
+    protected DownloadListener getDownloadListener() {
+        return new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String s1, String s2, String s3, long l) {
+                if (!TextUtils.isEmpty(url)) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                }
+            }
+        };
     }
 
     protected void setWebSettings(WebSettings webSettings) {
